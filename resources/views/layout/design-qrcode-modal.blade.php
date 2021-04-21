76b32d4bacfa11ff6a01b4ef5ab40ab61be13402
venkatesh <venkateshjuly1997@gmail.com>
2021-04-21 07:26:18 +0530

    changes for the options of barcode

---
+++ resources/views/layout/design-qrcode-modal.blade.php
@@ -0,0 +1,150 @@
+<div class="modal" id="desgin_qrcode_modal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+    <div class="modal-dialog modal-lg modal-dialog-centered" role="document">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="exampleModalLabel">Customize QR Code</h5>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close"> <span aria-hidden="true">&times;</span> </button>
+            </div>
+            <div class="modal-body">
+                <div class="row">
+                    <div class="col-md-12">
+                        <ul class="nav popup_gntr_qrcode_setting_nav" id="gntr-tab-1" role="tablist">
+                            <li class="nav-item"> <a class="nav-link active" id="gntr_prebuilt_tab-1" data-toggle="pill" href="#gntr_qrshapes_1" role="tab" aria-controls="gntr_qrshapes_1" aria-selected="true">QR Shapes</a> </li>
+                            <li class="nav-item"> <a class="nav-link" id="gntr_prebuilt_tab-1" data-toggle="pill" href="#gntr_prebuilt_1" role="tab" aria-controls="gntr_prebuilt_1" aria-selected="true">Pre-Designed</a> </li>
+                            <li class="nav-item"> <a class="nav-link" id="gntr_sticker_tab-1" data-toggle="pill" href="#gntr_sticker_1" role="tab" aria-controls="gntr_sticker_1" aria-selected="false">Stickers</a> </li>
+                            <li class="nav-item"> <a class="nav-link" id="gntr_color_tab-1" data-toggle="pill" href="#gntr_color_1" role="tab" aria-controls="pills-profile" aria-selected="false">Colors</a> </li>
+                            <li class="nav-item"> <a class="nav-link" id="gntr_shape_tab-1" data-toggle="pill" href="#gntr_shape_1" role="tab" aria-controls="pills-contact" aria-selected="false">Shapes</a> </li>
+                            <li class="nav-item"> <a class="nav-link" id="gntr_logo_tab-1" data-toggle="pill" href="#gntr_logo_1" role="tab" aria-controls="pills-contact" aria-selected="false">Logos</a> </li>
+                            <li class="nav-item"> <a class="nav-link" id="gntr_logo_tab-1" data-toggle="pill" href="#gntr_bgimg_1" role="tab" aria-controls="pills-contact" aria-selected="false">Decorate Your Picture</a> </li>
+                        </ul>
+                    </div>
+                </div>
+                <div class="row">
+                    <!-- popup left -->
+                    <div class="col-md-7">
+                        <div class="tab-content popup_gntr_tab_content">
+                            <!-- qrshapes -->
+                            <div class="tab-pane fade show active" id="gntr_qrshapes_1" role="tabpanel" aria-labelledby="gntr_prebuilt_tab-1">
+                                <ul class="popup_gntr_sticker_list">
+                                    <li class="js_qr_sticker" data-index="0">
+                                        <div class="close_sticker js_close_stiker"><i class="icon-cross"></i></div> <img loading="lazy" src="assets/images/sticker/sticker-1.svg" /> </li>
+                                    <li class="js_qr_sticker" data-index="1">
+                                        <div class="close_sticker js_close_stiker"><i class="icon-cross"></i></div> <img loading="lazy" src="assets/images/sticker/sticker-2.svg" /> </li>
+                                    <li class="js_qr_sticker" data-index="2">
+                                        <div class="close_sticker js_close_stiker"><i class="icon-cross"></i></div> <img loading="lazy" src="assets/images/sticker/sticker-3.svg" /> </li>
+                                </ul>
+                            </div>
+                            <!-- prebuilt -->
+                            <div class="tab-pane fade show" id="gntr_prebuilt_1" role="tabpanel" aria-labelledby="gntr_prebuilt_tab-1">
+                                <ul class="popup_gntr_sticker_list">
+                                    <li class="js_qr_sticker" data-index="0">
+                                        <div class="close_sticker js_close_stiker"><i class="icon-cross"></i></div> <img loading="lazy" src="assets/images/sticker/sticker-1.svg" /> </li>
+                                    <li class="js_qr_sticker" data-index="1">
+                                        <div class="close_sticker js_close_stiker"><i class="icon-cross"></i></div> <img loading="lazy" src="assets/images/sticker/sticker-2.svg" /> </li>
+                                    <li class="js_qr_sticker" data-index="2">
+                                        <div class="close_sticker js_close_stiker"><i class="icon-cross"></i></div> <img loading="lazy" src="assets/images/sticker/sticker-3.svg" /> </li>
+                                </ul>
+                            </div>
+                            <!-- sticker -->
+                            <div class="tab-pane fade" id="gntr_sticker_1" role="tabpanel" aria-labelledby="gntr_sticker_tab-1">
+                                <ul class="popup_gntr_sticker_list">
+                                    <li class="js_qr_sticker" data-index="0">
+                                        <div class="close_sticker js_close_stiker"><i class="icon-cross"></i></div> <img loading="lazy" src="assets/images/sticker/sticker-1.svg" /> </li>
+                                    <li class="js_qr_sticker" data-index="1">
+                                        <div class="close_sticker js_close_stiker"><i class="icon-cross"></i></div> <img loading="lazy" src="assets/images/sticker/sticker-2.svg" /> </li>
+                                    <li class="js_qr_sticker" data-index="2">
+                                        <div class="close_sticker js_close_stiker"><i class="icon-cross"></i></div> <img loading="lazy" src="assets/images/sticker/sticker-3.svg" /> </li>
+                                </ul>
+                            </div>
+                            <!-- color -->
+                            <div class="tab-pane fade" id="gntr_color_1" role="tabpanel" aria-labelledby="gntr_color_tab-1">
+                                <ul class="popup_gntr_color_list">
+                                    <li>
+                                        <div class="js_qr_color qr_color active">
+                                            <div class="qr_color_code" style="background: #000000"></div>
+                                        </div>
+                                    </li>
+                                    <li>
+                                        <div class="js_qr_color qr_color">
+                                            <div class="qr_color_code" style="background: #4d78fa"></div>
+                                        </div>
+                                    </li>
+                                    <li>
+                                        <div class="js_qr_color qr_color">
+                                            <div class="qr_color_code" style="background: #ff9000"></div>
+                                        </div>
+                                    </li>
+                                    <li>
+                                        <div class="js_qr_color qr_color">
+                                            <div class="qr_color_code" style="background: #e50041"></div>
+                                        </div>
+                                    </li>
+                                    <li>
+                                        <div class="qr_color change_qr_color js_change_qr_color d-none">
+                                            <div class="qr_color_code" style="background: #fff"> + </div>
+                                        </div>
+                                    </li>
+                                </ul>
+                            </div>
+                            <!-- shapes -->
+                            <div class="tab-pane fade" id="gntr_shape_1" role="tabpanel" aria-labelledby="gntr_shape_tab-1">
+                                <ul class="gntr_shape_list">
+                                    <li class="js_gntr_shape js_gntr_shape_default active"><img src="assets/images/qr-code-round.svg" /></li>
+                                    <li class="js_gntr_shape js_gntr_shape_circle"><img src="assets/images/qr-code-circle.svg" /></li>
+                                    <li class="js_gntr_shape js_gntr_shape_circle_2"><img src="assets/images/qr-code-circle-2.svg" /></li>
+                                </ul>
+                                
+                            </div>
+                            <!-- logo -->
+                            <div class="tab-pane fade" id="gntr_logo_1" role="tabpanel" aria-labelledby="gntr_logo_tab-1">
+                                <ul class="gntr_logo_list">
+                                    <li class="active gntr_logo_none js_qr_logo"><i class="icon-cross"></i></li>
+                                    <li class="gntr_logo_scanme_cor js_qr_logo" data-index="0"><i class="icon-scanme-cor"></i></li>
+                                    <li class="gntr_logo_scanme js_qr_logo" data-index="1"><i class="icon-scanme"></i></li>
+                                    <li class="gntr_logo_scanme js_qr_logo d-none" data-index="2"><i class="icon-plus_icon"></i></li>
+                                </ul>
+                                <div id="uploadLogo">
+                                    <label for="logoAddPic" class="custom-logoAddPic">    
+                                        <i class="icon-plus"></i> Add Your Own Logo
+                                    </label>
+                                    <input style="display:none" name="imagefile[]" type="file" id="logoAddPic" accept="image/*" onchange="loadLogoImage(event, logoUploaded)">
+                                </div>
+                            </div>
+                            <!-- shapes -->
+                            <div class="tab-pane fade" id="gntr_bgimg_1" role="tabpanel" aria-labelledby="gntr_shape_tab-1">
+                                <div class="subheading">Add QR Code to Your Own Picture</div>
+                                <div class="dflex items">
+                                    <div id="imageDiv" style="max-height:600px;max-width:600px;position:relative; display: inline-block; "> <img loading="lazy" id="qrcode2Img" style="margin-top: 0; max-height:600px;max-width:600px;" class="responsive-img" src="#">
+                                        <div id="resize-drag" class="ui-widget-content"></div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <!-- popup right -->
+                    <div class="col-md-5">
+                        <div class="popup_qr_preview_div">
+                            <div class="popup_qr_preview"> <img loading="lazy" id="qrcode" src="assets/images/qr-code.png"> </div> <a href="#" class="btn btn-default m-auto d-block show_size">More Sizes</a>
+                            <br/> <a href="#" id="download" class="btn btn-primary w-100 gntr_download_btn">Download Large Size <i
+                                        class="icon-down-arrow ml-2"></i></a> </div>
+                        <div class="qr_page_pre_more_popup" style="display: none;">
+                            <div class="row d-flex justify-content-between p-3">
+                                <div>Download Size</div>
+                                <div>
+                                    <button class="btn btn-sm btn-default"><i class="icon-cross"></i></button>
+                                </div>
+                            </div>
+                            <ul class="list-group secondary_color download_qr_size">
+                                <li class="list-group-item mt-2"><a href="#" data-size="4096">4096 X 4096</a></li>
+                                <li class="list-group-item mt-2"><a href="#" data-size="2048">2048 X 2048</a></li>
+                                <li class="list-group-item mt-2"><a href="#" data-size="1024">1024 X 1024</a></li>
+                                <li class="list-group-item"><a href="#" data-size="512">512 X 512</a></li>
+                                <li class="list-group-item"><a href="#" data-size="256">256 X 256</a></li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
--- resources/views/sections/ajax-dynamic-qr-code.blade.php
+++ resources/views/sections/ajax-dynamic-qr-code.blade.php
@@ -1,5 +1,15 @@
-{!! QrCode::style('round')
-->eyeColor(0, 230, 20, 42, 20, 60, 232)
-->size(260)
-// ->format('png')
-->generate($url) !!}
+@if ($image === null)
+    {!! QrCode::style('round')
+    ->eyeColor(0, 230, 20, 42, 20, 60, 232)
+    ->size(260)
+    // ->format('png')
+    ->generate($url) !!}    
+@else
+    {!! QrCode::style('round')
+    ->eyeColor(0, 230, 20, 42, 20, 60, 232)
+    ->size(260)
+    // ->format('png')
+    ->merge('')
+    ->generate($url) !!}        
+    
+@endif
